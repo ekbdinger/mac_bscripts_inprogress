9ae858f8bbf595c312f851d33cbbd27310e834b6
Paul <paulbosox@yahoo.com>
2012-09-11 18:18:54 -0400

    added ability to install the database and reset the password of the admin

--- drupalsite_create.sh
+++ drupalsite_create.sh
@@ -3,15 +3,40 @@
 # created this on September 11th, 2012.  
 # want to essentially make it one command to create a new drupal site
 
+# this seems to be working as of 18:18 on September 11th, 2012
+
 echo "This will create a new Drupal installation on a Mac OS X computer" ;
-echo "";
-echo "";
 echo "What is the name of your new drupal site (no spaces)";
-echo "":
 echo " ---------------------------"
 
-read -p "Enter your selection:  " drupal_name;
+read -p "Enter your selection:  " drupal_root;
 echo " ";
-# echo $choice;  // this is for debugging purposesd
+# echo $drupal_name  // for debugging purposes.  Good until this point
+
+cd ~/sites/
+echo "Downloading Drupal..."
 echo " "
-echo $drupal_name
+echo " "
+drush dl drupal --drupal-project-rename=$drupal_root
+echo " "
+echo " "
+cd $drupal_root/
+sleep 1 
+pwd
+echo " Creating MySQL Database"
+echo " "
+echo " "
+drush si --db-url=mysql://root:root@localhost:8889/$drupal_root
+echo " site installed!"
+echo " "
+echo " "
+ls
+sleep 1
+drush upwd admin --password=admin
+echo " "
+echo " "
+echo "admin password changed"
+echo " "
+echo " "
+echo " Your new Drupal site is installed locally.  Happy Drupaling!"
+# echo " log onto your new site localhost:8888"
