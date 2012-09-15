f3bc5b054c211f209a1f6d19369e482d1b597f8d
Paul <paulbosox@yahoo.com>
2012-09-15 19:41:35 -0400

    removing these because the file is in the scripts folder now

--- ekb_extern_hd_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# just a simple script to rsync the files from the mac to the external HD
-
-rsync -avz --progress /Users/paul/backups/ /Volumes/WD\ Passport/mac/backups
-
-# i think this is right, running it starting around 18:09 on September 15th
